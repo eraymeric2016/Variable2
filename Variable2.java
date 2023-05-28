5cf542d7c1e34607b676afec0a031bd17207353b
eraymeric2016 <125488259+eraymeric2016@users.noreply.github.com>
2023-05-28 14:22:01 +0300

    Add files via upload

---
+++ Variable2.java
@@ -0,0 +1,66 @@
+package week_03;
+
+public class Variable2 {
+    public static void main(String[] args) {
+      /*
+       System.out.println("Adam");
+
+        System.out.println("Adam");
+        System.out.println("Adam");
+        System.out.println("Adam");
+        System.out.println("Adam");
+        System.out.println("Adam");
+        System.out.println("Adam");
+        System.out.println("Adam");
+        System.out.println("Adam");
+        System.out.println("Adam");
+        */
+
+        String name="ErayGozde";
+
+
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+
+        //addition
+
+        System.out.println(23+4);
+        System.out.println('C'+4);
+
+
+        System.out.println("-------------------");
+        //concatenation
+
+        System.out.println(""+13+4);
+        System.out.println("12"+4);
+
+        System.out.println("------------------");
+
+        String a=""+12;
+        String result= a+4;
+
+
+
+        System.out.println(12+5+""+5);
+        //1.35.06 da kaldım 3. video
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
